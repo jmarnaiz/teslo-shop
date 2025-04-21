bd9363a7c470bd7e9549880e81aa2785324f7092
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-04-21 18:47:37 +0200

    First version of product service and product card component

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductService } from '@products/services/product.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {
+  productService = inject(ProductService);
+
+  productsResource = rxResource({
+    request: () => ({}),
+    loader: ({ request }) => {
+      return this.productService.getProducts();
+    },
+  });
+}
