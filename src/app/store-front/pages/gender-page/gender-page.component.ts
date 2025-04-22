eb113bf60704714a63a7b631f1440dd069125702
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-04-22 12:54:32 +0200

    Gender page component

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { Gender } from '@products/interfaces/product.interface';
+import { ProductService } from '@products/services/product.service';
+import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
 
 @Component({
-  selector: 'app-gender-page',
-  imports: [],
+  selector: 'gender-page',
+  imports: [ProductCardComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+  // Aquí si debo hacerlo de forma reactiva porque
+  // navego dentro de la misma página y por tanto,
+  // debo estar atento a los cambios
+
+  genderType = Gender;
+
+  private readonly _route = inject(ActivatedRoute);
+  private readonly _productService = inject(ProductService);
+
+  gender = toSignal(this._route.params.pipe(map(({ gender }) => gender)));
+  // Route params: { "gender": "women" }
+
+  productsResource = rxResource({
+    request: () => ({ gender: this.gender() }),
+    loader: ({ request }) => {
+      const { gender } = request;
+      return this._productService.getProducts({ gender });
+    },
+  });
+}
