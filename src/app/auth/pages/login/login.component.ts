815f32a2232c1cc2d19604cff1cb0b0b74a3956f
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-04-28 10:41:42 +0200

    Auth Service and login/register components

---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+})
+export class LoginComponent {
+  private readonly _fb = inject(FormBuilder);
+  private readonly _authService = inject(AuthService);
+  private readonly _router = inject(Router);
+
+  hasError = signal<boolean>(false);
+  isPosting = signal<boolean>(false);
+
+  loginForm = this._fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this._showError();
+      return;
+    }
+
+    const { email, password } = this.loginForm.value;
+
+    this._authService.login(email!, password!).subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this._router.navigateByUrl('/');
+        return;
+      }
+
+      this._showError();
+    });
+  }
+
+  private _showError() {
+    this.hasError.set(true);
+    setTimeout(() => {
+      this.hasError.set(false);
+    }, 2000);
+  }
+}
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,95 @@
+import { HttpClient } from '@angular/common/http';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import type { AuthResponse } from '@auth/interfaces/auth-response.interface';
+import { User } from '@auth/interfaces/user.interface';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+const BASE_URL = environment.baseUrl;
+const TOKEN_KEY = 'token';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly _authStatus = signal<AuthStatus>('checking');
+  private readonly _user = signal<User | null>(null);
+  private _token = signal<string | null>(null);
+
+  private readonly _http = inject(HttpClient);
+
+  checkStatusResource = rxResource({
+    loader: () => this.checkAuthStatus(),
+  });
+
+  // recuerda que los computed signal son de solo lectura
+  authStatus = computed<AuthStatus>(() => {
+    return this._authStatus() === 'checking'
+      ? 'checking'
+      : this._user()
+      ? 'authenticated'
+      : 'not-authenticated';
+  });
+
+  user = computed(() => this._user());
+  token = computed(this._token);
+
+  login(email: string, password: string): Observable<boolean> {
+    return this._http
+      .post<AuthResponse>(BASE_URL.concat('/auth/login'), { email, password })
+      .pipe(
+        map((resp) => this._handleAuthSuccess(resp)),
+        catchError(({ error }) => this._handleAuthError(error))
+        // console.error('Error on Auth Service: ', error.message);
+        // this._user.set(null);
+        // this._token.set(null);
+        // this._authStatus.set('not-authenticated'); // Esto no harría falta por la señak computada
+        // return of(false);
+      );
+  }
+  checkAuthStatus(): Observable<boolean> {
+    const token = localStorage.getItem(TOKEN_KEY);
+    if (!token) {
+      this.logout();
+      return of(false);
+    }
+
+    return this._http
+      .get<AuthResponse>(BASE_URL.concat('/auth/check-status'), {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .pipe(
+        map((resp) => this._handleAuthSuccess(resp)),
+        catchError(({ error }) => this._handleAuthError(error))
+      );
+  }
+
+  logout(): void {
+    this._user.set(null);
+    this._token.set(null);
+    this._authStatus.set('not-authenticated');
+
+    localStorage.removeItem(TOKEN_KEY);
+  }
+
+  private _handleAuthSuccess({ token, user }: AuthResponse): boolean {
+    this._user.set(user);
+    this._token.set(token);
+    this._authStatus.set('authenticated');
+
+    // Save token on local storage
+    localStorage.setItem(TOKEN_KEY, token);
+
+    return true;
+  }
+
+  private _handleAuthError(error: any): Observable<boolean> {
+    console.error('Error on Auth Service: ', error.message);
+    this.logout();
+    return of(false);
+  }
+}
