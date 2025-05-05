1bb5b7dfc2957a6d3e84709234f407cfe1150b9f
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-05-05 11:45:29 +0200

    Products admin page component

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductTableComponent } from '@products/components/product-table/product-table.component';
+import { ProductService } from '@products/services/product.service';
+import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { environment } from 'src/environments/environment';
+
+const PAGE_SIZE = environment.pageSize;
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [],
+  imports: [ProductTableComponent, PaginationComponent],
   templateUrl: './products-admin-page.component.html',
 })
-export class ProductsAdminPageComponent { }
+export class ProductsAdminPageComponent {
+  private readonly _productService = inject(ProductService);
+  readonly paginationService = inject(PaginationService);
+  productsPerPage = signal<number>(PAGE_SIZE);
+
+  productsResource = rxResource({
+    request: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productsPerPage(),
+    }),
+    loader: ({ request }) => {
+      return this._productService.getProducts({
+        offset: request.page * request.limit,
+        limit: request.limit,
+      });
+    },
+  });
+}
