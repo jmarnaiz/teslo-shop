7ccc363494d7d08b572f40d878c11e62fce82868
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-05-13 11:28:24 +0200

    Product Admin Page

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from '@products/services/product.service';
+import { map } from 'rxjs';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @Component({
   selector: 'app-product-admin-page',
-  imports: [],
+  imports: [ProductDetailsComponent],
   templateUrl: './product-admin-page.component.html',
 })
-export class ProductAdminPageComponent { }
+export class ProductAdminPageComponent {
+  private readonly _activatedRoute = inject(ActivatedRoute);
+  private readonly _productService = inject(ProductService);
+
+  private readonly _router = inject(Router);
+
+  productId = toSignal(
+    this._activatedRoute.params.pipe(map((params) => params['id']))
+  );
+
+  readonly productResource = rxResource({
+    request: () => ({ id: this.productId() }),
+    loader: ({ request }) => {
+      return this._productService.getProductById(request.id);
+    },
+  });
+
+  redirectEffect = effect(() => {
+    if (this.productResource.error()) {
+      this._router.navigateByUrl('admin/products');
+    }
+  });
+}
