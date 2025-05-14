66c61f5caed42a64d40c8f9c5949df4ed892f09c
Juan Manuel Arnaiz Caballero <jmanuelarnaiz@gmail.com>
2025-05-14 18:46:26 +0200

    Show placeholder image when no image is attached

--- src/app/products/pipes/product-image.pipe.ts
+++ src/app/products/pipes/product-image.pipe.ts
@@ -2,14 +2,18 @@ import { Pipe, type PipeTransform } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 const BASE_URL = environment.baseUrl;
+const NO_IMAGE_URL = 'assets/images/no-image.jpg';
 
 @Pipe({
   name: 'productImage',
 })
 export class ProductImagePipe implements PipeTransform {
-  transform(value: string | string[]): string {
+  transform(value: null | string | string[]): string {
     // if (Array.isArray(value) && value.length > 1) {
     // }
+    if (!value) {
+      return NO_IMAGE_URL;
+    }
 
     if (typeof value === 'string') {
       return `${BASE_URL}/files/product/${value}`;
@@ -18,7 +22,7 @@ export class ProductImagePipe implements PipeTransform {
     const image = value.at(0);
 
     if (!image) {
-      return 'assets/images/no-image.jpg';
+      return NO_IMAGE_URL;
     }
 
     return `${BASE_URL}/files/product/${image}`;
